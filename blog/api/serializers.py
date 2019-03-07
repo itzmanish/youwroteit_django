84ad9efa67c8fb925f17b43506127610d95029ae
Manish <itzmanish108@gmail.com>
2019-03-07 18:56:32 +0530

    permission changed of rest framework

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -8,6 +8,7 @@ class ArticleSerializer(serializers.ModelSerializer):
         model = Article
         fields = ('id', 'title', 'slug', 'content', 'created_at')
         # Important trick this will give me url like url/slug instead of url/id
+        # https://stackoverflow.com/questions/32201257/django-rest-framework-access-item-detail-by-slug-instead-of-id
         lookup_field = 'slug'
 
     # title = serializers.CharField()
