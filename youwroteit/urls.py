c7e5ff05ee02127c8d721f73e897a96e84872c55
Manish <12438068+itzmanish@users.noreply.github.com>
2019-04-19 18:43:07 +0530

    Update urls.py

--- youwroteit/urls.py
+++ youwroteit/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('articles/', include('blog.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
